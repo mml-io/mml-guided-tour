5a66617f0798c90cd9efdc80ffcc313134fa434b
TheCodeTherapy <me@mgz.me>
2024-08-09 14:02:21 +0100

    adds MML gliders example to Room 1

---
+++ packages/guided-tour/src/guided-tour/room-1.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/default
+import React from "react";
+
+import { gliders } from "../examples";
+
+export function Room1() {
+  return <m-frame src={gliders} x={-17.65} y={2.05} z={14} sy={2.63} sz={3.14}></m-frame>;
+}
--- packages/guided-tour/src/guided-tour/room.tsx
+++ packages/guided-tour/src/guided-tour/room.tsx
@@ -1,6 +1,8 @@
 // eslint-disable-next-line import/default
 import React from "react";
 
+import { Room1 } from "./room-1";
+
 type RoomType = {
   src: string;
   x: number;
@@ -10,16 +12,11 @@ type RoomType = {
 };
 
 export function Room({ src, x, y, z, index }: RoomType) {
-  switch (index) {
-    case 0: {
-      break;
-    }
-    default:
-      break;
-  }
+  const idx = index + 1;
   return (
     <m-group>
       <m-model src={src} x={x} y={y} z={z}></m-model>
+      {idx === 1 && <Room1 />}
     </m-group>
   );
 }
