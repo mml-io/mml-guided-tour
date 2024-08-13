12c17c937d56de1efee01d7dd8abc068432e8f11
TheCodeTherapy <me@mgz.me>
2024-08-13 11:00:16 +0100

    adds first interaction MML document

--- packages/guided-tour/src/examples/index.tsx
+++ packages/guided-tour/src/examples/index.tsx
@@ -1,5 +1,6 @@
 /*eslint import/no-unresolved: [2, { ignore: ['^mml:'] }]*/
 import dice from "mml:./dice.html";
+import firstInteraction from "mml:./first-interaction.html";
 import gliders from "mml:./gliders.html";
 
-export { dice, gliders };
+export { dice, gliders, firstInteraction };
