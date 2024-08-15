9920c047bffc2a0db583ceb5f7840f45c682c42e
TheCodeTherapy <me@mgz.me>
2024-08-15 16:50:23 +0100

    updates race cars react MML document

--- packages/guided-tour/src/guided-tour/react-examples/race-cars.tsx
+++ packages/guided-tour/src/guided-tour/react-examples/race-cars.tsx
@@ -1,18 +1,36 @@
-import { MAttrAnimElement, MAudioElement, MModelElement } from "@mml-io/mml-react-types";
+import { MAttrAnimElement } from "@mml-io/mml-react-types";
 import * as React from "react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import { TagCodeCanvas } from "../components/tag-code-canvas";
 import { randomArrayElement, randomInt } from "../helpers/js-helpers";
 import { useAttributes } from "../helpers/use-attributes";
 
+type PossibleEasings = "easeInOutQuad" | "easeInOutCubic" | "easeInOutQuart";
+type EasingsArray = Array<PossibleEasings>;
+
+type AnimationProps = {
+  winner?: boolean;
+  startTime: number;
+  endTime: number;
+  duration: number;
+  easing: PossibleEasings;
+};
+
+type AudioProps = {
+  volume: number;
+  startTime: number;
+  pauseTime: number;
+};
+
 export const RaceCars = ({ x, y, z, ry }: { x: number; y: number; z: number; ry: number }) => {
   const racing = useRef(false);
+
   const animRef = useRef<MAttrAnimElement | null>(null);
   const animAttributes = useAttributes(animRef);
 
-  const winner = useRef<string>("");
-  const winnerIndex = useRef<number>(-1);
+  const [animationProps, setAnimationProps] = useState<AnimationProps[]>([]);
+  const [audioProps, setAudioProps] = useState<AudioProps[]>([]);
 
   const raceDuration = 3900;
   const raceSoundDuration = 4000;
@@ -20,136 +38,103 @@ export const RaceCars = ({ x, y, z, ry }: { x: number; y: number; z: number; ry:
   const trackLength = 30;
   const carScale = 0.5;
 
-  const yellowAnimRef = useRef<MAttrAnimElement | null>(null);
-  const greenAnimRef = useRef<MAttrAnimElement | null>(null);
-  const blueAnimRef = useRef<MAttrAnimElement | null>(null);
-  const pinkAnimRef = useRef<MAttrAnimElement | null>(null);
-  const redAnimRef = useRef<MAttrAnimElement | null>(null);
-  const aimRefs = [yellowAnimRef, greenAnimRef, blueAnimRef, pinkAnimRef, redAnimRef];
-
-  const yellowCarRef = useRef<MModelElement | null>(null);
-  const greenCarRef = useRef<MModelElement | null>(null);
-  const blueCarRef = useRef<MModelElement | null>(null);
-  const pinkCarRef = useRef<MModelElement | null>(null);
-  const redCarRef = useRef<MModelElement | null>(null);
-  const carRefs = [yellowCarRef, greenCarRef, blueCarRef, pinkCarRef, redCarRef];
-
-  const yellowSoundRef = useRef<MAudioElement | null>(null);
-  const greenSoundRef = useRef<MAudioElement | null>(null);
-  const blueSoundRef = useRef<MAudioElement | null>(null);
-  const pinkSoundRef = useRef<MAudioElement | null>(null);
-  const redSoundRef = useRef<MAudioElement | null>(null);
-  const carSoundRefs = [yellowSoundRef, greenSoundRef, blueSoundRef, pinkSoundRef, redSoundRef];
-
-  const easings = ["easeInOutQuad", "easeInOutCubic", "easeInOutQuart"];
+  const easings: EasingsArray = ["easeInOutQuad", "easeInOutCubic", "easeInOutQuart"];
   const availableCars = ["yellow", "green", "blue", "pink", "red"];
-
-  const availableCarsModels: Record<string, string> = {
-    yellow: "/assets/guidedtour/f1_race_car_yellow.glb",
-    green: "/assets/guidedtour/f1_race_car_green.glb",
-    blue: "/assets/guidedtour/f1_race_car_blue.glb",
-    pink: "/assets/guidedtour/f1_race_car_pink.glb",
-    red: "/assets/guidedtour/f1_race_car_red.glb",
-  };
-
-  // const colorSaturation = "100%";
-  // const colorLightness = "65%";
-  // const carColors = [
-  //   `hsl(72, ${colorSaturation}, ${colorLightness})`,
-  //   `hsl(144, ${colorSaturation}, ${colorLightness})`,
-  //   `hsl(216, ${colorSaturation}, ${colorLightness})`,
-  //   `hsl(288, ${colorSaturation}, ${colorLightness})`,
-  //   `hsl(380, ${colorSaturation}, ${colorLightness})`,
-  // ];
-
-  const setAnimRef = React.useCallback((animElement: MAttrAnimElement) => {
-    animRef.current = animElement;
-  }, []);
+  const availableCarsModels: Record<string, string> = {};
+  for (let i = 0; i < availableCars.length; i++) {
+    availableCarsModels[availableCars[i]] =
+      `/assets/guidedtour/f1_race_car_${availableCars[i]}.glb`;
+  }
 
   const animateCars = () => {
     if (racing.current) return;
     racing.current = true;
-    let smallestTime = 999999999999;
+
+    let smallestTime = Infinity;
+    let winnerIndex = -1;
+
     const now = document.timeline.currentTime as number;
 
-    availableCars.forEach((_car, index) => {
-      const car = carRefs[index].current;
-      const carSound = carSoundRefs[index].current;
-      const animElement = aimRefs[index].current;
+    const newAudioProps: AudioProps[] = [];
 
-      if (!car || !carSound || !animElement) return;
-
+    const newAnimationProps = availableCars.map((_car, index) => {
       const time = randomInt(raceDuration - raceDuration * 0.1, raceDuration);
-      const easing = randomArrayElement(easings);
-
-      animElement.setAttribute("attr", "x");
-      animElement.setAttribute("start", `${-trackLength / 2 + 1}`);
-      animElement.setAttribute("end", `${trackLength / 2 - 1.5}`);
-      animElement.setAttribute("start-time", `${now}`);
-      animElement.setAttribute("end-time", `${now + time}`);
-      animElement.setAttribute("duration", `${time}`);
-      animElement.setAttribute("loop", "false");
-      animElement.setAttribute("ping-pong", "false");
-      animElement.setAttribute("easing", easing);
-
-      const audioOffset = easing === "easeInOutQuad" ? 50 : easing === "easeInOutCubic" ? 250 : 350;
-      carSound.setAttribute("volume", "1");
-      carSound.setAttribute("start-time", `${now + audioOffset}`);
-      carSound.setAttribute("pause-time", `${now + audioOffset + raceSoundDuration}`);
-
-      setTimeout(() => {
-        car.setAttribute("x", `${trackLength / 2 - 1.5}`);
-        carSound.setAttribute("volume", "0");
-      }, time);
+      const easing: PossibleEasings = randomArrayElement(easings);
 
       if (time < smallestTime) {
         smallestTime = time;
-        winner.current = `Winner: ${availableCars[index]} (${(time / 1000).toFixed(3)}s)`;
-        winnerIndex.current = index;
-        setAnimRef(animElement);
+        winnerIndex = index;
       }
+
+      const offset = easing === "easeInOutQuad" ? 50 : easing === "easeInOutCubic" ? 250 : 350;
+
+      newAudioProps.push({
+        volume: 1,
+        startTime: now + offset,
+        pauseTime: now + offset + raceSoundDuration,
+      });
+
+      return {
+        winner: false,
+        startTime: now,
+        endTime: now + time,
+        duration: time,
+        easing,
+      };
     });
+
+    newAnimationProps[winnerIndex].winner = true;
+    setAnimationProps(newAnimationProps);
+    setAudioProps(newAudioProps);
     setTimeout(() => (racing.current = false), raceDuration + 100);
   };
 
-  const Cars = () => (
-    <m-group id="cars">
-      {availableCars.map((car, index) => (
-        <m-model
-          ref={carRefs[index]}
-          id={car}
-          key={index}
-          src={availableCarsModels[car] as string}
-          x={-trackLength / 2 + 1}
-          y={0.3}
-          z={index * carScale * 2 - carScale * 0.75}
-          sx={carScale}
-          sy={carScale}
-          sz={carScale}
-        >
-          <m-attr-anim
-            ref={aimRefs[index]}
-            id={`${car}-anim`}
-            attr="x"
-            start={-trackLength / 2 + 1}
-            end={trackLength / 2 - 1.5}
-            start-time={0}
-            duration={0}
-            loop={false}
-            easing="easeInOutQuad"
-          ></m-attr-anim>
-          <m-audio
-            ref={carSoundRefs[index]}
-            src="/assets/guidedtour/sfx_racing.mp3"
-            start-time={-raceSoundDuration}
-            pause-time={document.timeline.currentTime as number}
-            loop={false}
-            volume={0}
-          ></m-audio>
-        </m-model>
-      ))}
-    </m-group>
-  );
+  const Cars = ({ animations, audio }: { animations: AnimationProps[]; audio: AudioProps[] }) => {
+    for (let i = 0; i < animations.length; i++) {
+      if (animations[i].winner) {
+        console.log(animations[i]);
+      }
+    }
+    return (
+      <m-group id="cars">
+        {availableCars.map((car, index) => (
+          <m-model
+            id={car}
+            key={index}
+            src={availableCarsModels[car] as string}
+            x={-trackLength / 2 + 1}
+            y={0.3}
+            z={index * carScale * 2 - carScale * 0.75}
+            sx={carScale}
+            sy={carScale}
+            sz={carScale}
+          >
+            {animations.length === availableCars.length && (
+              <m-attr-anim
+                ref={animations[index].winner ? animRef : undefined}
+                attr="x"
+                start={-trackLength / 2 + 1}
+                end={trackLength / 2 - 1.5}
+                start-time={animations[index].startTime}
+                duration={animations[index].duration}
+                loop={false}
+                easing={animations[index].easing}
+              ></m-attr-anim>
+            )}
+            {audio.length === availableCars.length && (
+              <m-audio
+                src="/assets/guidedtour/sfx_racing.mp3"
+                start-time={audio[index].startTime}
+                pause-time={audio[index].pauseTime}
+                loop={false}
+                volume={audio[index].volume}
+              ></m-audio>
+            )}
+          </m-model>
+        ))}
+      </m-group>
+    );
+  };
 
   const RaceTrack = () => (
     <m-group id="race-track">
@@ -184,7 +169,7 @@ export const RaceCars = ({ x, y, z, ry }: { x: number; y: number; z: number; ry:
       }}
     >
       <RaceTrack />
-      <Cars />
+      <Cars animations={animationProps} audio={audioProps} />
       <m-group x={-5} z={4.4} ry={180}>
         <m-model
           src="/assets/guidedtour/code_display.glb"
