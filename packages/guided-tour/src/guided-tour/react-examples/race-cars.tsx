640a3ec60d7ea481928aa78ddee36a4fd92da941
TheCodeTherapy <me@mgz.me>
2024-08-15 13:47:50 +0100

    updates race cars react MML document

--- packages/guided-tour/src/guided-tour/react-examples/race-cars.tsx
+++ packages/guided-tour/src/guided-tour/react-examples/race-cars.tsx
@@ -1,10 +1,12 @@
-import { MModelElement } from "@mml-io/mml-react-types";
+import { MAudioElement, MModelElement } from "@mml-io/mml-react-types";
 import * as React from "react";
-import { useCallback } from "react";
+import { useCallback, useMemo, useRef } from "react";
 
-import { randomArrayElement } from "../helpers/js-helpers";
+import { randomArrayElement, randomInt } from "../helpers/js-helpers";
 
 export const RaceCars = ({ x, y, z, ry }: { x: number; y: number; z: number; ry: number }) => {
+  const racing = useRef(false);
+
   const raceDuration = 3900;
   const raceSoundDuration = 4000;
   const countdownSoundDuration = 4000;
@@ -19,10 +21,21 @@ export const RaceCars = ({ x, y, z, ry }: { x: number; y: number; z: number; ry:
   const blueCarRef = React.useRef<MModelElement | null>(null);
   const pinkCarRef = React.useRef<MModelElement | null>(null);
   const redCarRef = React.useRef<MModelElement | null>(null);
-  const carRefs = [yellowCarRef, greenCarRef, blueCarRef, pinkCarRef, redCarRef];
+  const carRefs = useMemo(() => [yellowCarRef, greenCarRef, blueCarRef, pinkCarRef, redCarRef], []);
 
-  const easings = ["easeInOutQuad", "easeInOutCubic"];
-  const availableCars = ["yellow", "green", "blue", "pink", "red"];
+  const yellowCarSoundRef = React.useRef<MAudioElement | null>(null);
+  const greenCarSoundRef = React.useRef<MAudioElement | null>(null);
+  const blueCarSoundRef = React.useRef<MAudioElement | null>(null);
+  const pinkCarSoundRef = React.useRef<MAudioElement | null>(null);
+  const redCarSoundRef = React.useRef<MAudioElement | null>(null);
+  const carSoundRefs = useMemo(
+    () => [yellowCarSoundRef, greenCarSoundRef, blueCarSoundRef, pinkCarSoundRef, redCarSoundRef],
+    [],
+  );
+
+  const easings = useMemo(() => ["easeInOutQuad", "easeInOutCubic", "easeInOutQuart"], []);
+  const availableCars = useMemo(() => ["yellow", "green", "blue", "pink", "red"], []);
+
   const availableCarsModels: Record<string, string> = {
     yellow: "/assets/guidedtour/f1_race_car_yellow.glb",
     green: "/assets/guidedtour/f1_race_car_green.glb",
@@ -38,26 +51,47 @@ export const RaceCars = ({ x, y, z, ry }: { x: number; y: number; z: number; ry:
     `hsl(380, ${colorSaturation}, ${colorLightness})`,
   ];
 
-  const createAnimation = useCallback(
-    (startTime: number, endTime: number) => {
-      return (
-        <m-attr-anim
-          attr="x"
-          start={-trackLength / 2 + 1}
-          end={trackLength / 2 - 1.5}
-          start-time={startTime}
-          end-time={endTime}
-          loop={false}
-          ping-pong={false}
-          duration="5000"
-          easing={randomArrayElement(easings)}
-        ></m-attr-anim>
-      );
-    },
-    [easings],
-  );
+  const animateCars = useCallback(() => {
+    if (racing.current) return;
+    racing.current = true;
+    let smallestTime = 999999999999;
+    const now = document.timeline.currentTime as number;
 
-  const animateCars = useCallback(() => {}, []);
+    availableCars.forEach((_car, index) => {
+      const car = carRefs[index].current;
+      const carSound = carSoundRefs[index].current;
+      if (!car || !carSound) return;
+
+      const time = randomInt(raceDuration - raceDuration * 0.1, raceDuration);
+      const easing = randomArrayElement(easings);
+
+      const animation = document.createElement("m-attr-anim");
+      animation.setAttribute("attr", "x");
+      animation.setAttribute("start", `${-trackLength / 2 + 1}`);
+      animation.setAttribute("end", `${trackLength / 2 - 1.5}`);
+      animation.setAttribute("start-time", `${now}`);
+      animation.setAttribute("end-time", `${now + time}`);
+      animation.setAttribute("duration", `${time}`);
+      animation.setAttribute("loop", "false");
+      animation.setAttribute("ping-pong", "false");
+      animation.setAttribute("easing", easing);
+      car.appendChild(animation);
+
+      const audioOffset = easing === "easeInOutQuad" ? 50 : easing === "easeInOutCubic" ? 250 : 350;
+      carSound.setAttribute("volume", "1");
+      carSound.setAttribute("start-time", `${now + audioOffset}`);
+      carSound.setAttribute("pause-time", `${now + audioOffset + raceSoundDuration}`);
+      setTimeout(() => {
+        car.setAttribute("x", `${trackLength / 2 - 1.5}`);
+        car.removeChild(animation);
+        carSound.setAttribute("volume", "0");
+      }, time);
+      if (time < smallestTime) {
+        smallestTime = time;
+      }
+    });
+    setTimeout(() => (racing.current = false), raceDuration + 100);
+  }, [availableCars, carRefs, carSoundRefs, easings]);
 
   const Cars = () => (
     <m-group id="cars">
@@ -73,7 +107,16 @@ export const RaceCars = ({ x, y, z, ry }: { x: number; y: number; z: number; ry:
           sx={carScale}
           sy={carScale}
           sz={carScale}
-        ></m-model>
+        >
+          <m-audio
+            ref={carSoundRefs[index]}
+            src="/assets/guidedtour/sfx_racing.mp3"
+            start-time={-raceSoundDuration}
+            pause-time={document.timeline.currentTime as number}
+            loop={false}
+            volume={0}
+          ></m-audio>
+        </m-model>
       ))}
     </m-group>
   );
@@ -101,7 +144,15 @@ export const RaceCars = ({ x, y, z, ry }: { x: number; y: number; z: number; ry:
   );
 
   return (
-    <m-group x={x} y={y} z={z} ry={ry}>
+    <m-group
+      x={x}
+      y={y}
+      z={z}
+      ry={ry}
+      onClick={() => {
+        animateCars();
+      }}
+    >
       <RaceTrack />
       <Cars />
     </m-group>
