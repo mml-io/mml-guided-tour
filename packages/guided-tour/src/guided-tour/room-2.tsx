4563079d06cc16674c6003f4a2880a46155dfc92
TheCodeTherapy <me@mgz.me>
2024-08-16 13:41:10 +0100

    removes unecessary props for room's components

--- packages/guided-tour/src/guided-tour/room-2.tsx
+++ packages/guided-tour/src/guided-tour/room-2.tsx
@@ -6,8 +6,8 @@ import { RotatingCarPlinth } from "./react-examples/rotating-car-plinth";
 export function Room2({ x, y, z }: { x: number; y: number; z: number }) {
   return (
     <m-group x={x} y={y} z={z}>
-      <RotatingCarPlinth x={11} y={0} z={0} />
-      <RaceCars x={-12.5} y={0} z={-2} ry={-90} />
+      <RotatingCarPlinth x={11} />
+      <RaceCars x={-12.5} z={-2} ry={-90} />
     </m-group>
   );
 }
