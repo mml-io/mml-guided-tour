26348f848ca0bd5f72d13447bcc1d46aa125fcd9
TheCodeTherapy <me@mgz.me>
2024-08-19 12:21:56 +0100

    makes some components memoized to avoid unecessary re-renders

--- packages/guided-tour/src/guided-tour/components/tag-code-canvas.tsx
+++ packages/guided-tour/src/guided-tour/components/tag-code-canvas.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useCallback, useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 
 type TagCodeCanvasProps = {
   tagAttributes: Record<string, string>;
@@ -9,110 +9,107 @@ type TagCodeCanvasProps = {
   tag: string;
 };
 
-export function TagCodeCanvas({
-  tagAttributes,
-  fontSize,
-  color,
-  emissive,
-  tag,
-}: TagCodeCanvasProps): JSX.Element {
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
+export const TagCodeCanvas = memo(
+  ({ tagAttributes, fontSize, color, emissive, tag }: TagCodeCanvasProps) => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
 
-  const [lineHeight, setLineHeight] = useState<number>(0);
-  const [canvasFontSize] = useState<number>(fontSize);
+    const [lineHeight, setLineHeight] = useState<number>(0);
+    const [canvasFontSize] = useState<number>(fontSize);
 
-  const [imageWidth, setImageWidth] = useState<number>(0);
-  const [imageHeight, setImageHeight] = useState<number>(0);
+    const [imageWidth, setImageWidth] = useState<number>(0);
+    const [imageHeight, setImageHeight] = useState<number>(0);
 
-  const [openingTag] = useState<string>(`<${tag}`);
-  const [closingTag] = useState<string>(`></${tag}>`);
+    const [openingTag] = useState<string>(`<${tag}`);
+    const [closingTag] = useState<string>(`></${tag}>`);
 
-  const [imageSrc, setImageSrc] = useState<string>("");
-  const [imageEmissive] = useState<number>(emissive);
+    const [imageSrc, setImageSrc] = useState<string>("");
+    const [imageEmissive] = useState<number>(emissive);
 
-  const defaultColor = color || "#ffffff";
-  const attributeColor = "#ff77aa";
-  const eqColor = "#777777";
-  const valueColor = "#33aaff";
+    const defaultColor = color || "#ffffff";
+    const attributeColor = "#ff77aa";
+    const eqColor = "#777777";
+    const valueColor = "#33aaff";
 
-  const treatedValue = (value: string | number): string => {
-    const numberValue = parseFloat(value as string);
-    return !isNaN(numberValue)
-      ? numberValue.toFixed(2)
-      : (value as string).replace("/assets/guidedtour", "https://mml.io");
-  };
+    const treatedValue = (value: string | number): string => {
+      const numberValue = parseFloat(value as string);
+      return !isNaN(numberValue)
+        ? numberValue.toFixed(2)
+        : (value as string).replace("/assets/guidedtour", "https://mml.io");
+    };
 
-  const paintCanvas = useCallback(() => {
-    ctx!.font = `900 ${canvasFontSize}px monospace`;
-    let yPos = lineHeight;
-    ctx!.fillStyle = defaultColor;
-    ctx!.fillText(openingTag, 0, yPos);
-    yPos += lineHeight;
-    Object.entries(tagAttributes).forEach(([attribute, attributeValue]) => {
-      let xOffset = canvasFontSize * 0.6;
-      const value = treatedValue(attributeValue);
-      ctx!.fillStyle = attributeColor;
-      ctx!.fillText(` ${attribute}`, xOffset, yPos);
-      xOffset += canvasFontSize * (attribute.length + 2) * 0.6;
-      ctx!.fillStyle = eqColor;
-      ctx!.fillText("=", xOffset, yPos);
-      xOffset += canvasFontSize * 2 * 0.6;
-      ctx!.fillStyle = valueColor;
-      ctx!.fillText(value, xOffset, yPos);
+    const paintCanvas = useCallback(() => {
+      ctx!.font = `900 ${canvasFontSize}px monospace`;
+      let yPos = lineHeight;
+      ctx!.fillStyle = defaultColor;
+      ctx!.fillText(openingTag, 0, yPos);
       yPos += lineHeight;
-      xOffset = canvasFontSize;
-    });
-    ctx!.fillStyle = defaultColor;
-    ctx!.fillText(closingTag, 0, yPos);
-    const dataUrl = canvas.toDataURL("image/png");
-    setImageSrc(dataUrl);
-  }, [
-    canvas,
-    closingTag,
-    ctx,
-    defaultColor,
-    canvasFontSize,
-    lineHeight,
-    openingTag,
-    tagAttributes,
-  ]);
+      Object.entries(tagAttributes).forEach(([attribute, attributeValue]) => {
+        let xOffset = canvasFontSize * 0.6;
+        const value = treatedValue(attributeValue);
+        ctx!.fillStyle = attributeColor;
+        ctx!.fillText(` ${attribute}`, xOffset, yPos);
+        xOffset += canvasFontSize * (attribute.length + 2) * 0.6;
+        ctx!.fillStyle = eqColor;
+        ctx!.fillText("=", xOffset, yPos);
+        xOffset += canvasFontSize * 2 * 0.6;
+        ctx!.fillStyle = valueColor;
+        ctx!.fillText(value, xOffset, yPos);
+        yPos += lineHeight;
+        xOffset = canvasFontSize;
+      });
+      ctx!.fillStyle = defaultColor;
+      ctx!.fillText(closingTag, 0, yPos);
+      const dataUrl = canvas.toDataURL("image/png");
+      setImageSrc(dataUrl);
+    }, [
+      canvas,
+      closingTag,
+      ctx,
+      defaultColor,
+      canvasFontSize,
+      lineHeight,
+      openingTag,
+      tagAttributes,
+    ]);
 
-  const getElementAttributes = useCallback(() => {
-    setLineHeight(parseFloat((canvasFontSize * 1.05).toFixed(2)));
+    const getElementAttributes = useCallback(() => {
+      setLineHeight(parseFloat((canvasFontSize * 1.05).toFixed(2)));
 
-    let maxCharacters = 0;
-    Object.entries(tagAttributes).forEach(([attribute, attributeValue]) => {
-      const value = treatedValue(attributeValue);
-      if (attribute.length + value.length + 1 > maxCharacters) {
-        maxCharacters = attribute.length + value.length + 1;
-      }
-    });
-    const width = (maxCharacters + 5) * canvasFontSize * 0.6;
-    const height = canvasFontSize * (Object.keys(tagAttributes).length + 2) * 1.25;
-    canvas.width = width;
-    canvas.height = height;
-    setImageWidth(Math.round(width * 100) / 10000);
-    setImageHeight(Math.round(height * 100) / 10000);
-    ctx!.fillStyle = "rgba(0, 0, 0, 0)";
-    ctx!.fillRect(0, 0, width, height);
+      let maxCharacters = 0;
+      Object.entries(tagAttributes).forEach(([attribute, attributeValue]) => {
+        const value = treatedValue(attributeValue);
+        if (attribute.length + value.length + 1 > maxCharacters) {
+          maxCharacters = attribute.length + value.length + 1;
+        }
+      });
+      const width = (maxCharacters + 5) * canvasFontSize * 0.6;
+      const height = canvasFontSize * (Object.keys(tagAttributes).length + 2) * 1.25;
+      canvas.width = width;
+      canvas.height = height;
+      setImageWidth(Math.round(width * 100) / 10000);
+      setImageHeight(Math.round(height * 100) / 10000);
+      ctx!.fillStyle = "rgba(0, 0, 0, 0)";
+      ctx!.fillRect(0, 0, width, height);
 
-    paintCanvas();
-  }, [canvasFontSize, tagAttributes, canvas, ctx, paintCanvas]);
+      paintCanvas();
+    }, [canvasFontSize, tagAttributes, canvas, ctx, paintCanvas]);
 
-  useEffect(() => {
-    getElementAttributes();
-  }, [getElementAttributes, tagAttributes]);
+    useEffect(() => {
+      getElementAttributes();
+    }, [getElementAttributes, tagAttributes]);
 
-  return (
-    <m-group>
-      <m-image
-        src={imageSrc}
-        width={imageWidth}
-        height={imageHeight}
-        emissive={imageEmissive}
-        collide={false}
-      ></m-image>
-    </m-group>
-  );
-}
+    return (
+      <m-group>
+        <m-image
+          src={imageSrc}
+          width={imageWidth}
+          height={imageHeight}
+          emissive={imageEmissive}
+          collide={false}
+        ></m-image>
+      </m-group>
+    );
+  },
+);
+TagCodeCanvas.displayName = "TagCodeCanvas";
--- packages/guided-tour/src/guided-tour/react-examples/race-cars.tsx
+++ packages/guided-tour/src/guided-tour/react-examples/race-cars.tsx
@@ -1,6 +1,6 @@
 import { MAttrAnimElement } from "@mml-io/mml-react-types";
 import * as React from "react";
-import { useRef, useState } from "react";
+import { memo, useRef, useState } from "react";
 
 import { PlayButton } from "../components/play-button";
 import { TagCodeCanvas } from "../components/tag-code-canvas";
@@ -31,6 +31,141 @@ type RaceCarsProps = {
   ry?: number;
 };
 
+const raceDuration = 3900;
+const raceSoundDuration = 4000;
+
+const trackLength = 30;
+const carScale = 0.5;
+
+const colorSaturation = "100%";
+const colorLightness = "65%";
+const carColors = [
+  `hsl(72, ${colorSaturation}, ${colorLightness})`,
+  `hsl(144, ${colorSaturation}, ${colorLightness})`,
+  `hsl(216, ${colorSaturation}, ${colorLightness})`,
+  `hsl(288, ${colorSaturation}, ${colorLightness})`,
+  `hsl(380, ${colorSaturation}, ${colorLightness})`,
+];
+
+const easings: EasingsArray = ["easeInOutQuad", "easeInOutCubic", "easeInOutQuart"];
+const availableCars = ["yellow", "green", "blue", "pink", "red"];
+const availableCarsModels: Record<string, string> = {};
+for (let i = 0; i < availableCars.length; i++) {
+  availableCarsModels[availableCars[i]] = `/assets/guidedtour/f1_race_car_${availableCars[i]}.glb`;
+}
+
+const countDownAudioURL = "/assets/guidedtour/sfx_countdown.mp3";
+const CountDownSound = memo(
+  ({ startTime, pauseTime, volume }: { startTime: number; pauseTime: number; volume: number }) => (
+    <m-audio
+      src={countDownAudioURL}
+      start-time={startTime}
+      pause-time={pauseTime}
+      loop={false}
+      volume={volume}
+    ></m-audio>
+  ),
+);
+CountDownSound.displayName = "CountDownSound";
+
+const CountDown = memo(({ countDown, show }: { countDown: string; show: boolean }) => (
+  <m-group>
+    <m-label
+      content={countDown}
+      font-size={210}
+      width={5.5}
+      height={3.0}
+      y={2.2}
+      x={-0.5}
+      z={show ? -0.1 : 0.1}
+      alignment="center"
+      padding={0}
+      color="#000000"
+      font-color="#aaeeaa"
+      emissive={7}
+    ></m-label>
+  </m-group>
+));
+CountDown.displayName = "CountDown";
+
+const Cars = memo(
+  ({
+    animations,
+    audio,
+    animRef,
+  }: {
+    animations: AnimationProps[];
+    audio: AudioProps[];
+    animRef: React.MutableRefObject<MAttrAnimElement | null>;
+  }) => {
+    return (
+      <m-group id="cars">
+        {availableCars.map((car, index) => (
+          <m-model
+            key={`car-${index}`}
+            id={car}
+            src={availableCarsModels[car] as string}
+            x={-trackLength / 2 + 1}
+            y={0.3}
+            z={index * carScale * 2 - carScale * 0.75}
+            sx={carScale}
+            sy={carScale}
+            sz={carScale}
+          >
+            {animations.length === availableCars.length && (
+              <m-attr-anim
+                key={`anim-${index}`}
+                ref={animations[index].winner ? animRef : null}
+                attr="x"
+                start={-trackLength / 2 + 1}
+                end={trackLength / 2 - 1.5}
+                start-time={animations[index].startTime}
+                duration={animations[index].duration}
+                loop={false}
+                easing={animations[index].easing}
+              ></m-attr-anim>
+            )}
+            {audio.length === availableCars.length && (
+              <m-audio
+                key={`audio-${index}`}
+                src="/assets/guidedtour/sfx_racing.mp3"
+                start-time={audio[index].startTime}
+                pause-time={audio[index].pauseTime}
+                loop={false}
+                volume={audio[index].volume}
+              ></m-audio>
+            )}
+          </m-model>
+        ))}
+      </m-group>
+    );
+  },
+);
+Cars.displayName = "Cars";
+
+const RaceTrack = memo(() => (
+  <m-group id="race-track">
+    <m-cube
+      color="#424242"
+      width={trackLength}
+      height={0.1}
+      depth={carScale * 10}
+      y={0.25}
+      z={carScale * 3.1}
+    >
+      <m-image
+        src="/assets/guidedtour/texture_checkered.jpg"
+        width={carScale * 10}
+        x={trackLength / 2 - 1.4}
+        y={0.066}
+        rx={-90}
+        rz={90}
+      ></m-image>
+    </m-cube>
+  </m-group>
+));
+RaceTrack.displayName = "RaceTrack";
+
 export function RaceCars({ x, y, z, ry }: RaceCarsProps): JSX.Element {
   const racing = useRef(false);
 
@@ -43,29 +178,10 @@ export function RaceCars({ x, y, z, ry }: RaceCarsProps): JSX.Element {
   const [attributes, setAttributes] = useState<Record<string, string>>({});
   const [winnerIndex, setWinnerIndex] = useState<number>(0);
 
-  const raceDuration = 3900;
-  const raceSoundDuration = 4000;
-
-  const trackLength = 30;
-  const carScale = 0.5;
-
-  const colorSaturation = "100%";
-  const colorLightness = "65%";
-  const carColors = [
-    `hsl(72, ${colorSaturation}, ${colorLightness})`,
-    `hsl(144, ${colorSaturation}, ${colorLightness})`,
-    `hsl(216, ${colorSaturation}, ${colorLightness})`,
-    `hsl(288, ${colorSaturation}, ${colorLightness})`,
-    `hsl(380, ${colorSaturation}, ${colorLightness})`,
-  ];
-
-  const easings: EasingsArray = ["easeInOutQuad", "easeInOutCubic", "easeInOutQuart"];
-  const availableCars = ["yellow", "green", "blue", "pink", "red"];
-  const availableCarsModels: Record<string, string> = {};
-  for (let i = 0; i < availableCars.length; i++) {
-    availableCarsModels[availableCars[i]] =
-      `/assets/guidedtour/f1_race_car_${availableCars[i]}.glb`;
-  }
+  const [countDown, setCountDown] = useState<string>("");
+  const [countDownStart, setCountDownStart] = useState<number>(0);
+  const [countDownPause, setCountDownPause] = useState<number>(0);
+  const [countDownVolume, setCountDownVolume] = useState<number>(0);
 
   const animateCars = () => {
     if (racing.current) return;
@@ -120,100 +236,59 @@ export function RaceCars({ x, y, z, ry }: RaceCarsProps): JSX.Element {
     setTimeout(() => (racing.current = false), raceDuration + 100);
   };
 
-  const Cars = ({ animations, audio }: { animations: AnimationProps[]; audio: AudioProps[] }) => {
-    return (
-      <m-group id="cars">
-        {availableCars.map((car, index) => (
-          <m-model
-            key={`car-${index}`}
-            id={car}
-            src={availableCarsModels[car] as string}
-            x={-trackLength / 2 + 1}
-            y={0.3}
-            z={index * carScale * 2 - carScale * 0.75}
-            sx={carScale}
-            sy={carScale}
-            sz={carScale}
-          >
-            {animations.length === availableCars.length && (
-              <m-attr-anim
-                key={`anim-${index}`}
-                ref={animations[index].winner ? animRef : null}
-                attr="x"
-                start={-trackLength / 2 + 1}
-                end={trackLength / 2 - 1.5}
-                start-time={animations[index].startTime}
-                duration={animations[index].duration}
-                loop={false}
-                easing={animations[index].easing}
-              ></m-attr-anim>
-            )}
-            {audio.length === availableCars.length && (
-              <m-audio
-                key={`audio-${index}`}
-                src="/assets/guidedtour/sfx_racing.mp3"
-                start-time={audio[index].startTime}
-                pause-time={audio[index].pauseTime}
-                loop={false}
-                volume={audio[index].volume}
-              ></m-audio>
-            )}
-          </m-model>
-        ))}
-      </m-group>
-    );
+  const startCountDown = () => {
+    setCountDownStart(document.timeline.currentTime as number);
+    setCountDownPause((document.timeline.currentTime as number) + 4000);
+    setCountDownVolume(1);
+    setTimeout(() => setCountDown("3"), 400);
+    setTimeout(() => setCountDown("2"), 1300);
+    setTimeout(() => setCountDown("1"), 2300);
+    setTimeout(() => setCountDown("GO"), 3250);
+    setTimeout(() => animateCars(), 3250);
+    setTimeout(() => setCountDown(""), 3250 + raceDuration);
   };
-
-  const RaceTrack = () => (
-    <m-group id="race-track">
-      <m-cube
-        color="#424242"
-        width={trackLength}
-        height={0.1}
-        depth={carScale * 10}
-        y={0.25}
-        z={carScale * 3.1}
-      >
-        <m-image
-          src="/assets/guidedtour/texture_checkered.jpg"
-          width={carScale * 10}
-          x={trackLength / 2 - 1.4}
-          y={0.066}
-          rx={-90}
-          rz={90}
-        ></m-image>
-      </m-cube>
-    </m-group>
-  );
 
   return (
     <m-group x={x} y={y} z={z} ry={ry}>
-      <PlayButton
-        x={-7}
-        z={-2}
-        reEnableTime={raceDuration + 100}
-        callback={() => {
-          animateCars();
-        }}
-      />
-      <RaceTrack />
-      <Cars animations={animationProps} audio={audioProps} />
-      <m-group x={-5} z={4.4} ry={180}>
-        <m-model
-          src="/assets/guidedtour/code_display.glb"
-          sx={0.6}
-          sy={0.6}
-          sz={0.5}
-          ry={180}
-        ></m-model>
-        <m-group x={-0.3} y={2.03}>
-          <TagCodeCanvas
-            tagAttributes={attributes}
-            fontSize={30}
-            color={carColors[winnerIndex]}
-            emissive={12}
-            tag="m-attr-anim"
+      <m-group>
+        <PlayButton
+          x={-7}
+          z={-2}
+          reEnableTime={raceDuration + 100}
+          callback={() => {
+            startCountDown();
+          }}
+        />
+        <RaceTrack />
+        <m-group x={-5} z={4.4} ry={180}>
+          <m-model
+            src="/assets/guidedtour/code_display.glb"
+            sx={0.6}
+            sy={0.6}
+            sz={0.5}
+            ry={180}
+          ></m-model>
+          <CountDown
+            countDown={countDown}
+            show={countDown === "" && Object.keys(attributes).length !== 0}
           />
+          <CountDownSound
+            startTime={countDownStart}
+            pauseTime={countDownPause}
+            volume={countDownVolume}
+          />
+          <m-group x={-0.3} y={2.03}>
+            <TagCodeCanvas
+              tagAttributes={attributes}
+              fontSize={30}
+              color={carColors[winnerIndex]}
+              emissive={12}
+              tag="m-attr-anim"
+            />
+          </m-group>
+        </m-group>
+        <m-group>
+          <Cars animations={animationProps} audio={audioProps} animRef={animRef} />
         </m-group>
       </m-group>
     </m-group>
