2fb103138ec703ef3c19d251c6317f1631b72916
TheCodeTherapy <me@mgz.me>
2024-08-20 15:34:21 +0100

    adds memory game react MML document to room 2

--- packages/guided-tour/src/guided-tour/room-2.tsx
+++ packages/guided-tour/src/guided-tour/room-2.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+import { MemoryGame } from "./react-examples/memory-game";
 import { RaceCars } from "./react-examples/race-cars";
 import { RotatingCarPlinth } from "./react-examples/rotating-car-plinth";
 
@@ -8,6 +9,9 @@ export function Room2() {
     <m-group>
       <RotatingCarPlinth x={11} />
       <RaceCars x={-12.5} z={-4} ry={-90} />
+      <m-group x={5.5} y={1.79} z={21.7} ry={180} sx={1.25} sy={1.25} sz={1.25}>
+        <MemoryGame />
+      </m-group>
     </m-group>
   );
 }
