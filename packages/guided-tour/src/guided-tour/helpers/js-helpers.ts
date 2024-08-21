79b2b92fa2dffcd33676d4c16b4279d07df5ce0d
TheCodeTherapy <me@mgz.me>
2024-08-21 14:29:18 +0100

    adds helper to compose visibility strings from a players Set

--- packages/guided-tour/src/guided-tour/helpers/js-helpers.ts
+++ packages/guided-tour/src/guided-tour/helpers/js-helpers.ts
@@ -8,3 +8,7 @@ export const randomInt = (min: number, max: number): number => {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const setToCSVString = (set: Set<number>) => {
+  return Array.from(set).join(" ");
+};
