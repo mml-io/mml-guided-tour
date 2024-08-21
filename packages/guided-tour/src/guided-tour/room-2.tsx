42995ef4a7dc3d12c3ab5518f55aa4d7f26fc24f
TheCodeTherapy <me@mgz.me>
2024-08-21 14:32:37 +0100

    updates room-2 document to use the useVisibilityProbe hook

--- packages/guided-tour/src/guided-tour/room-2.tsx
+++ packages/guided-tour/src/guided-tour/room-2.tsx
@@ -1,16 +1,27 @@
+import { MGroupElement, MPositionProbeElement } from "@mml-io/mml-react-types";
 import * as React from "react";
+import { useRef } from "react";
 
+import { useVisibilityProbe } from "./helpers/use-visibility-probe";
 import { MemoryGame } from "./react-examples/memory-game";
 import { RaceCars } from "./react-examples/race-cars";
 import { RotatingCarPlinth } from "./react-examples/rotating-car-plinth";
 
 export function Room2() {
+  const probeRef = useRef<MPositionProbeElement | null>(null);
+  const groupRef = useRef<MGroupElement | null>(null);
+
+  useVisibilityProbe(probeRef, groupRef, 32, 500);
+
   return (
     <m-group>
-      <RotatingCarPlinth x={11} />
-      <RaceCars x={-12.5} z={-4} ry={-90} />
-      <m-group x={5.5} y={1.79} z={21.7} ry={180} sx={1.25} sy={1.25} sz={1.25}>
-        <MemoryGame />
+      <m-position-probe ref={probeRef} />
+      <m-group ref={groupRef}>
+        <RotatingCarPlinth x={11} />
+        <RaceCars x={-12.5} z={-4} ry={-90} />
+        <m-group x={5.5} z={21.7} ry={180}>
+          <MemoryGame />
+        </m-group>
       </m-group>
     </m-group>
   );
