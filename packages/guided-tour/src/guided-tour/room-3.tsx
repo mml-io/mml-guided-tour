aaf1015f422a6c99e6484ae321196584b90b3ffb
TheCodeTherapy <me@mgz.me>
2024-08-22 19:52:51 +0100

    adds gaming video document to room 3

--- packages/guided-tour/src/guided-tour/room-3.tsx
+++ packages/guided-tour/src/guided-tour/room-3.tsx
@@ -1,5 +1,22 @@
+import { MGroupElement, MPositionProbeElement } from "@mml-io/mml-react-types";
 import * as React from "react";
+import { useRef } from "react";
+
+import { useVisibilityProbe } from "./helpers/use-visibility-probe";
+import { GamingVideo } from "./react-examples/gaming-video";
 
 export function Room3() {
-  return <m-group></m-group>;
+  const probeRef = useRef<MPositionProbeElement | null>(null);
+  const groupRef = useRef<MGroupElement | null>(null);
+
+  useVisibilityProbe(probeRef, groupRef, 32, 500);
+
+  return (
+    <m-group>
+      <m-position-probe ref={probeRef} />
+      <m-group ref={groupRef}>
+        <GamingVideo x={10} z={10} />
+      </m-group>
+    </m-group>
+  );
 }
