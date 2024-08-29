5b7a13533a4ba121aacf5600904f09e302f0295d
TheCodeTherapy <me@mgz.me>
2024-08-29 15:41:45 +0100

    adds a teleporter component to room 5

--- packages/guided-tour/src/guided-tour/room-5.tsx
+++ packages/guided-tour/src/guided-tour/room-5.tsx
@@ -1,5 +1,20 @@
 import * as React from "react";
 
+import { Teleporter } from "./components/teleporter";
+
 export function Room5() {
-  return <m-group></m-group>;
+  return (
+    <m-group>
+      <Teleporter
+        startX={-10}
+        startY={0}
+        startZ={-10}
+        startRY={180}
+        endX={10}
+        endY={0}
+        endZ={10}
+        endRY={0}
+      />
+    </m-group>
+  );
 }
