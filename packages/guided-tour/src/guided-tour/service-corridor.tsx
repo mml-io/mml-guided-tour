7bdb09e29a8ccadfc171816b5d5fae69e358bdd1
TheCodeTherapy <me@mgz.me>
2024-09-04 17:06:34 +0100

    implements a service corridor document

---
+++ packages/guided-tour/src/guided-tour/service-corridor.tsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import { memo } from "react";
+
+type ServiceCorridorProps = {
+  x: number;
+  y: number;
+  z: number;
+  width: number;
+  height: number;
+  depth: number;
+  wallColor: string;
+  glassColor: string;
+  glassOpacity: number;
+};
+export const ServiceCorridor = memo(
+  ({
+    x,
+    y,
+    z,
+    width,
+    height,
+    depth,
+    wallColor,
+    glassColor,
+    glassOpacity,
+  }: ServiceCorridorProps) => {
+    const wallThickness = 0.1;
+    const closeGapDistance = 4.75;
+    return (
+      <m-group x={x} y={y} z={z}>
+        <m-cube
+          id="ground"
+          x={-width / 2 - closeGapDistance}
+          y={-wallThickness}
+          width={width}
+          height={wallThickness}
+          depth={depth}
+          color={wallColor}
+        ></m-cube>
+        <m-cube
+          id="start-wall"
+          x={-width / 2 - closeGapDistance}
+          z={-depth / 2}
+          y={height / 2}
+          width={width}
+          height={height}
+          color={wallColor}
+        ></m-cube>
+        <m-cube
+          id="end-wall"
+          x={-width / 2 - closeGapDistance}
+          z={depth / 2}
+          y={height / 2}
+          width={width}
+          height={height}
+          color={wallColor}
+        ></m-cube>
+        <m-cube
+          id="glass-wall"
+          x={-width - closeGapDistance}
+          y={height / 2 - wallThickness - 1.5}
+          width={width - closeGapDistance}
+          height={wallThickness}
+          depth={depth}
+          opacity={glassOpacity}
+          rz={90}
+          color={glassColor}
+          cast-shadows={false}
+        ></m-cube>
+        <m-cube
+          id="column"
+          x={-width - closeGapDistance + 0.5}
+          y={height - wallThickness - 0.5}
+          width={1}
+          height={1}
+          depth={depth}
+          opacity={1}
+          rz={90}
+          color={wallColor}
+        ></m-cube>
+        <m-cube
+          id="ceiling"
+          x={-width / 2 - closeGapDistance - 0.5}
+          y={height - wallThickness}
+          width={width - 0.5}
+          height={wallThickness}
+          depth={depth}
+          opacity={glassOpacity}
+          color={glassColor}
+          cast-shadows={false}
+        ></m-cube>
+      </m-group>
+    );
+  },
+);
+ServiceCorridor.displayName = "ServiceCorridor";
