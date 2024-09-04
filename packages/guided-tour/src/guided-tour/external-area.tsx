55b3e3d3df1b75796e9aa4412a4bcf3135618378
TheCodeTherapy <me@mgz.me>
2024-09-04 14:38:54 +0100

    sets the proper position for the MML logo document

--- packages/guided-tour/src/guided-tour/external-area.tsx
+++ packages/guided-tour/src/guided-tour/external-area.tsx
@@ -11,7 +11,7 @@ type ExternalAreaProps = {
 export function ExternalArea({ x, y, z }: ExternalAreaProps) {
   return (
     <m-group x={x} y={y} z={z}>
-      <MMLLogo x={0} y={-2.5} z={13.5} sx={0.65} sy={0.65} sz={0.65} />
+      <MMLLogo x={-30} y={-2.5} z={13.5} sx={0.65} sy={0.65} sz={0.65} />
       <TrackingDuck x={-25} y={5} z={67.5} />
     </m-group>
   );
