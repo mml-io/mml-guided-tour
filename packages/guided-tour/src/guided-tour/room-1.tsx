8b82c04ee81e3ff801b3c1ff5aff8ce88df8aecc
TheCodeTherapy <me@mgz.me>
2024-09-04 13:38:57 +0100

    adds conditional load/unload to objects on room 1

--- packages/guided-tour/src/guided-tour/room-1.tsx
+++ packages/guided-tour/src/guided-tour/room-1.tsx
@@ -1,12 +1,23 @@
+import { MGroupElement, MPositionProbeElement } from "@mml-io/mml-react-types";
 import * as React from "react";
+import { useRef } from "react";
 
+import { useVisibilityProbe } from "./helpers/use-visibility-probe";
 import { firstInteraction, gliders } from "../examples";
 
 export function Room1() {
+  const probeRef = useRef<MPositionProbeElement | null>(null);
+  const groupRef = useRef<MGroupElement | null>(null);
+
+  useVisibilityProbe(probeRef, groupRef, 32, 500);
+
   return (
     <m-group>
-      <m-frame src={firstInteraction}></m-frame>
-      <m-frame src={gliders} x={-17.65} y={2.05} sy={2.63} sz={3.14}></m-frame>
+      <m-position-probe ref={probeRef} />
+      <m-group ref={groupRef}>
+        <m-frame src={firstInteraction}></m-frame>
+        <m-frame src={gliders} x={-17.65} y={2.05} sy={2.63} sz={3.14}></m-frame>
+      </m-group>
     </m-group>
   );
 }
