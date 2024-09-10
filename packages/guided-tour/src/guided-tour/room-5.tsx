f7ca2c7347819f117b40ab70cc26fae1545685c2
TheCodeTherapy <me@mgz.me>
2024-09-10 10:59:59 +0100

    adds the glass bridge game and teleporter to room 5

--- packages/guided-tour/src/guided-tour/room-5.tsx
+++ packages/guided-tour/src/guided-tour/room-5.tsx
@@ -1,35 +1,44 @@
-import { MGroupElement, MPositionProbeElement } from "@mml-io/mml-react-types";
 import * as React from "react";
-import { useRef } from "react";
 
 import { Teleporter } from "./components/teleporter";
-import { useVisibilityProbe } from "./helpers/use-visibility-probe";
+import { GlassBridgeGame } from "./react-examples/glass-bridge";
 import { PlatformerGame } from "./react-examples/platformer-game";
 
 export function Room5() {
-  const probeRef = useRef<MPositionProbeElement | null>(null);
-  const groupRef = useRef<MGroupElement | null>(null);
-
-  useVisibilityProbe(probeRef, groupRef, 32, 500);
-
   const platformerGamePosX = 1000;
   const platformerGamePosY = 10000;
 
+  const glassBrigdeGamePosX = -1000;
+  const glassBrigdeGamePosY = 15000;
+
   return (
     <m-group>
-      <Teleporter
-        startX={-15}
-        startY={0}
-        startZ={-10.75}
-        startRY={180}
-        endX={platformerGamePosX}
-        endY={platformerGamePosY}
-        endZ={-5}
-        endRY={90}
-      />
-      <PlatformerGame x={platformerGamePosX} y={platformerGamePosY} />
-      <m-position-probe ref={probeRef} />
-      <m-group ref={groupRef}></m-group>
+      <m-group id="platformer-game-wrapper">
+        <Teleporter
+          startX={-15}
+          startY={0}
+          startZ={-10.75}
+          startRY={180}
+          endX={platformerGamePosX}
+          endY={platformerGamePosY}
+          endZ={-5}
+          endRY={90}
+        />
+        <PlatformerGame x={platformerGamePosX} y={platformerGamePosY} />
+      </m-group>
+      <m-group id="glass-bridge-game-wrapper">
+        <Teleporter
+          startX={-15}
+          startY={0}
+          startZ={2.25}
+          startRY={180}
+          endX={glassBrigdeGamePosX}
+          endY={glassBrigdeGamePosY}
+          endZ={-5}
+          endRY={90}
+        />
+        <GlassBridgeGame x={glassBrigdeGamePosX} y={glassBrigdeGamePosY} />
+      </m-group>
     </m-group>
   );
 }
