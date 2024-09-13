a31bf0bbbbc824382b497ae684012c7870b0481b
TheCodeTherapy <me@mgz.me>
2024-09-13 12:40:19 +0100

    adds call to action room to rooms root

--- packages/guided-tour/src/guided-tour/rooms-root.tsx
+++ packages/guided-tour/src/guided-tour/rooms-root.tsx
@@ -5,6 +5,7 @@ import { Room2 } from "./room-2";
 import { Room3 } from "./room-3";
 import { Room4 } from "./room-4";
 import { Room5 } from "./room-5";
+import { Room6 } from "./room-6";
 
 type RoomType = {
   src: string;
@@ -24,6 +25,7 @@ export function RoomsRoot({ src, x, y, z, index }: RoomType) {
       {idx === 3 && <Room3 />}
       {idx === 4 && <Room4 />}
       {idx === 5 && <Room5 />}
+      {idx === 6 && <Room6 />}
     </m-group>
   );
 }
