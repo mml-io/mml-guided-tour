c130855d67a581060554e8c9b8d5bfa2b4980758
Daniel J. Rollins <me@deej.io>
2024-09-30 15:45:41 +0100

    add github action to deploy code to mserve and assets to gcs

--- packages/guided-tour/src/helpers/render-as-mml.ts
+++ packages/guided-tour/src/helpers/render-as-mml.ts
@@ -2,8 +2,7 @@ import { flushSync } from "react-dom";
 import { createRoot } from "react-dom/client";
 
 export function renderAsMML(node: React.ReactNode) {
-  const container =
-    document.getElementById("root") ?? document.body.appendChild(document.createElement("div"));
+  const container = document.body.appendChild(document.createElement("div"));
   const root = createRoot(container);
   flushSync(() => {
     root.render(node);
