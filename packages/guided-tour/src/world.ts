6e6f2e6c7bf4e5d98838888b0d77ce22d4d3dcfb
TheCodeTherapy <me@mgz.me>
2024-10-22 17:40:13 +0100

    updates world config to include avatar configuration settings

--- packages/guided-tour/src/world.ts
+++ packages/guided-tour/src/world.ts
@@ -2,10 +2,20 @@ import type { MMLWorldConfig } from "@mml-io/esbuild-plugin-mml";
 /*eslint import/no-unresolved: [2, { ignore: ['^mml:'] }]*/
 import tour from "mml:./guided-tour";
 
+import botModel from "./assets/models/bot.glb";
+import botThumbnail from "./assets/models/thumbs/bot.jpg";
+
 export default {
   mmlDocuments: {
     tour: {
       url: tour,
     },
   },
+  avatars: [
+    {
+      name: "Bot",
+      meshFileUrl: botModel,
+      thumbnailUrl: botThumbnail,
+    },
+  ],
 } satisfies MMLWorldConfig;
